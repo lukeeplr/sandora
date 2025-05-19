3b22ba332538f11637fb52e20dabda279e2c944f
Lucas <lukeeplr@gmail.com>
2025-05-19 12:01:24 -0300

    fix: nav container overlaping form

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -3,8 +3,8 @@ import Logo from './Logo'
 
 function NavBar() {
   return (
-    <nav className='hidden lg:flex fixed w-full h-40 py-8 px-16 items-center'>
-        <Logo className='w-50 h-10' />
+    <nav className='hidden lg:flex fixed w-full h-40 py-8 px-16 items-center pointer-events-none'>
+        <Logo className='w-50 h-10 pointer-events-auto' />
     </nav>
   )
 }
