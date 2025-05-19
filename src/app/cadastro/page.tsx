37baed3c45bd33e241fcc76447acf61c3ed47aa9
Lucas <lukeeplr@gmail.com>
2025-05-19 16:49:59 -0300

    fix: missing text margins

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Page() {
   return (
   <>
-    <section className='hidden lg:flex items-center justify-center px-16'>
+    <section className='hidden lg:flex items-center justify-center px-16 mr-auto'>
       <div>
         <h1 className='font-bold text-5xl text-dark-neutral'>Crie sua conta e comece a <span className='text-brand'>aprender</span></h1>
         <p className='max-w-[40ch] mt-4 font-medium text-xl text-light-neutral'><span className='text-brand'>Crie sua conta em poucos segundos</span> e comece sua jornada de aprendizado.</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "@/components/forms/LoginForm";
 export default function Home() {
   return (
     <>
-    <section className='hidden lg:flex items-center justify-center px-16'>
+    <section className='hidden lg:flex items-center justify-center px-16 text-left mr-auto'>
       <div>
       <h1 className='font-bold text-5xl text-dark-neutral'>Bem-vindo(a) de volta!</h1>
       <p className='max-w-[40ch] mt-4 font-medium text-xl text-medium-neutral'>Faça login com seu e-mail  para acessar seus cursos.</p>
